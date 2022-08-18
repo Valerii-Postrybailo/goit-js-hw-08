8354a5e8c473e73d6e1331dda5cb684751a26d75
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-08-19 00:13:46 +0300

    second task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,9 +4,32 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-//////////////////////
+////////////////////////////////////////////////////
 
 // Описаний в документації
-// import SimpleLightbox from "simplelightbox";
-// // Додатковий імпорт стилів
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+////////////////////////////////////////////////////
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery)
+
+const imgMarkup = createGalleryMarkup(galleryItems)
+
+gallery.insertAdjacentHTML('afterbegin', imgMarkup)
+
+function createGalleryMarkup(items) { 
+  return items
+    .map(({ preview, original, description }) => {
+      return`
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"  title ="${description}"/>
+      </a>
+  `})
+    .join("")
+}
+
+  new SimpleLightbox('.gallery a', {captionDelay	:250 });
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const lastSavedTime = localStorage.getItem("videoplayer-current-time");
+console.log(lastSavedTime);
+
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+}, 1000));
+
+
+player.setCurrentTime(lastSavedTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
