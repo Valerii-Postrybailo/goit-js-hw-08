816bc6973a55b13473b2156ce4abea896bc3b660
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-08-20 19:56:01 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+const userEmail = document.querySelector("input")
+console.log(userEmail)
+
+const userMessage = document.querySelector("textarea")
+console.log(userMessage)
+
+userEmail.addEventListener("input", (event) => {
+  console.log(event.currentTarget.value)
+  const userEmailJSON = JSON.stringify(event.currentTarget.value)
+    console.log(userEmailJSON)
+})
+
+  userMessage.addEventListener("input", (event) => {
+  console.log(event.currentTarget.value)
+  const userMessageJSON = JSON.stringify(event.currentTarget.value)
+    console.log(userMessageJSON)
+})
 
