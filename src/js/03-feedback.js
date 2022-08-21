45be8e7938fd9c9c75da7f4955866454929d227b
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-08-22 02:05:28 +0300

    refactored code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,91 @@
-const LOCAL_STORAGE_KEY = "feedback-form-state"
+// const LOCAL_STORAGE_KEY = "feedback-form-state"
 
-const refs = {
-  userEmail: document.querySelector('.input[name="email"]'),
-  userMessage: document.querySelector('textarea[name="message"]'),
-  form: document.querySelector('.feedback-form'),
-}
+// const refs = {
+//   userEmail: document.querySelector('.input[name="email"]'),
+//   userMessage: document.querySelector('textarea[name="message"]'),
+//   form: document.querySelector('.feedback-form'),
+// }
 
+// const userEmail = document.querySelector('.input[name="email"]')
+//   const  userMessage = document.querySelector('textarea[name="message"]')
+// const form = document.querySelector('.feedback-form')
+  
+// const dataBase = {
+//   email: '',
+//   message: '',
+// }
+
+
+// ////////////////////////////////////////////////////////
+
+// form.addEventListener('submit', onFormSubmit)
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   event.currentTarget.reset();
+
+//   console.log(dataBase);
+// }
+
+// function validateForm(event) {
+//   const { name, value } = event.target;
+//   dataBase[name] = value;
+//   console.log(dataBase[name])
+//   console.log("yyy suk")
+
+//   localStorage.setItem('feedback-form-state', JSON.stringify(dataBase));
+// }
+// ////////////////////////////////////////////////////////
+
+// function getDataBase(){
+// const getData = localStorage.getItem('feedback-form-state');
+// const parsedData = JSON.parse(getData);
+//   if (parsedData) {
+//   console.log(dataBase);
+
+//     console.log(parsedData.email)
+//     console.log(userEmail)
+//     userEmail.value = parsedData.email;
+//     userMessage.value = parsedData.message;
+//   }
+// }
+// form.addEventListener('input', validateForm);
+
+// getDataBase();
+
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('input[name="email"]');
+const messageRef = document.querySelector('textarea[name="message"]');
 const dataBase = {
-  email: '',
-  message: '',
+    email: '',
+    message: '',
 }
 
-refs.form.addEventListener('input', validateForm);
-
-////////////////////////////////////////////////////////
-
-refs.form.addEventListener('submit', onFormSubmit)
-
-function onFormSubmit(event) {
-  event.preventDefault();
-  event.currentTarget.reset();
-
-  console.log(dataBase);
+// ///////////////////////////////////////////////////////
+function validateForm(e){
+    const { name, value } = e.target;
+    dataBase[name] = value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(dataBase));
 }
-
-function validateForm(event) {
-  const { name, value } = event.target;
-  dataBase[name] = value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(dataBase));
+function submitForm (e){
+    e.preventDefault();
+    e.currentTarget.reset();
+    console.log(dataBase);
 }
-////////////////////////////////////////////////////////
-
 function getDataBase(){
 const getData = localStorage.getItem('feedback-form-state');
 const parsedData = JSON.parse(getData);
 if (parsedData) {
-    refs.userEmail.value = parsedData.dataBase.email;
-    refs.userMessage.value = parsedData.dataBase.message;
-  }
-}
+    emailRef.value = parsedData.email;
+    messageRef.value = parsedData.message;
+}}
+
+//////////////////////////////////////////////////
+formRef.addEventListener('submit', submitForm);
+
+formRef.addEventListener('input', validateForm);
 
 getDataBase();
-
 
 
 
