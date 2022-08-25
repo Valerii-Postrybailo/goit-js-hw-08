22c50371f654dd1e9e1a7412b827e8fe970d72af
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-08-25 19:12:22 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,29 +5,38 @@ const LOCAL_STORAGE_KEY = "feedback-form-state"
 const form = document.querySelector('.feedback-form');
 const userEmail = document.querySelector('input[name="email"]');
 const userMessage = document.querySelector('textarea[name="message"]');
-const dataBase = {
-  email: '',
-  message: '',
-}
+// const dataBase = {
+//   email: '',
+//   message: '',
+// }
+const formValues = {}
 
 /////////////////////////////////////////////////////////
 
-form.addEventListener('input', throttle(validateForm, 500));
+getDataBase();
 
+form.addEventListener('input', throttle(validateForm, 500));
 
 function validateForm(event){
   const { name, value } = event.target;
-  dataBase[name] = value;
-  console.log(dataBase[name])
+  // dataBase[name] = value;
+  // console.log(dataBase[name])
 
   let alreadyInStorage = localStorage.getItem(LOCAL_STORAGE_KEY)
 
   if (alreadyInStorage) {
     alreadyInStorage = JSON.parse(alreadyInStorage)
+  } else {
+      alreadyInStorage ={}
   }
   
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(dataBase));
+  alreadyInStorage[name] = value;
+  
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(alreadyInStorage));
 }
+  
+  // localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(dataBase));
+// }
 
 //////////////////////////////////////////////////////////
 
@@ -35,20 +44,36 @@ form.addEventListener('submit', submitForm);
 
 function submitForm (event){
   event.preventDefault();
-  event.currentTarget.reset();
-  console.log(dataBase);
-}
 
-getDataBase();
+  const {
+    elements: { email, message }
+  } = event.currentTarget;
+
+  if (email.value === "" || message.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${email.value}, Message: ${message.value}`);
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY)
+}
 
 function getDataBase(){
   const getData = localStorage.getItem(LOCAL_STORAGE_KEY);
   const parsedData = JSON.parse(getData);
   
 if (parsedData) {
-  userEmail.value = parsedData.email;
-  userMessage.value = parsedData.message;
+  // userEmail.value = parsedData.email || '';
+  // userMessage.value = parsedData.message || '';
+
+  console.log(parsedData)
+  Object.entries(parsedData).forEach(([name, value])=> {
+    // console.log(name, value)
+    formValues[name] = value;
+    form.elements[name].value = value
+  })
 }}
+
 
 
 
